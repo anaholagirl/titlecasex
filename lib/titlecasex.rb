aedd40ad8fb3de997b2e6757314c1aad8c6cc053
Linda Gibson & Patrick David <student@epicodus.com>
2014-07-23 14:00:11 -0700

    Capitalizes first letter of first word regardless of word. Added README file.

--- lib/titlecasex.rb
+++ lib/titlecasex.rb
@@ -2,16 +2,17 @@ def titlecasex(input)
   inputSplit = input.split
   i = 0
   newPhrase = []
-  while i < inputSplit.length do
-    if inputSplit[i] == "a" || inputSplit[i] == "the" || inputSplit[i] == "and" || inputSplit[i] == "but" || inputSplit[i] == "an" || inputSplit[i] == "to" ||
-      inputSplit[i] == "or"
-      inputSplit[i] == inputSplit[i].downcase
-      newPhrase.push(inputSplit[i])
-    else
-      w1 = inputSplit[i].upcase[0] + inputSplit[i].downcase[1..-1]
-      newPhrase.push(w1)
+    while i < inputSplit.length do
+      if inputSplit[i] == "a" || inputSplit[i] == "the" || inputSplit[i] == "and" || inputSplit[i] == "but" || inputSplit[i] == "an" || inputSplit[i] == "to" ||
+        inputSplit[i] == "or"
+        inputSplit[i] == inputSplit[i].downcase
+        newPhrase.push(inputSplit[i])
+      else
+        w1 = inputSplit[i].upcase[0] + inputSplit[i].downcase[1..-1]
+        newPhrase.push(w1)
+      end
+      i = i + 1
     end
-    i = i + 1
-  end
+  newPhrase[0]= newPhrase[0].upcase[0] + newPhrase[0].downcase[1..-1]
   newPhrase.join(' ')
 end
--- spec/titlecasex_spec.rb
+++ spec/titlecasex_spec.rb
@@ -13,4 +13,8 @@ describe('titlecasex') do
   it('will not capitalize words like "the", "and", "but, "an", and "to"') do
     expect(titlecasex("cat and mouse but a not or the to")).to(eq("Cat and Mouse but a Not or the to"))
   end
+
+  it('will capitalize the first letter of the first word even if it is one of the following: "the", "and", "but, "an", and "to"') do
+    expect(titlecasex("the cat and the hat")).to(eq("The Cat and the Hat"))
+  end
 end
