9c04378c893ad49b5f22d054d3b108fc3cdcd8d1
Linda Gibson & Patrick David <student@epicodus.com>
2014-07-23 13:34:35 -0700

    will not capitalize certain word articles

--- lib/titlecasex.rb
+++ lib/titlecasex.rb
@@ -3,10 +3,14 @@ def titlecasex(input)
   i = 0
   newPhrase = []
   while i < inputSplit.length do
-    w1 = inputSplit[i].upcase[0] + inputSplit[i].downcase[1..-1]
-    newPhrase.push(w1)
-  # w2 = inputSplit[1].upcase[0] + inputSplit[1].downcase[1..-1]
-  # w3 = inputSplit[2].upcase[0] + inputSplit[2].downcase[1..-1]
+    if inputSplit[i] == "a" || inputSplit[i] == "the" || inputSplit[i] == "and" || inputSplit[i] == "but" || inputSplit[i] == "an" || inputSplit[i] == "to" ||
+      inputSplit[i] == "or"
+      inputSplit[i] == inputSplit[i].downcase
+      newPhrase.push(inputSplit[i])
+    else
+      w1 = inputSplit[i].upcase[0] + inputSplit[i].downcase[1..-1]
+      newPhrase.push(w1)
+    end
     i = i + 1
   end
   newPhrase.join(' ')
--- spec/titlecasex_spec.rb
+++ spec/titlecasex_spec.rb
@@ -9,4 +9,8 @@ describe('titlecasex') do
   it('will capitalize the first letter of multiple words') do
     expect(titlecasex("cAt atE mouse")).to(eq("Cat Ate Mouse"))
   end
+
+  it('will not capitalize words like "the", "and", "but, "an", and "to"') do
+    expect(titlecasex("cat and mouse but a not or the to")).to(eq("Cat and Mouse but a Not or the to"))
+  end
 end
